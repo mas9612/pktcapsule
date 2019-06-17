3d86cfe04cccd0337185ec261915b712b39a1b56
Masato Yamazaki <mas9612@gmail.com>
2019-06-17 14:23:57 +0900

    implement method to deserialize IP header

--- ipinip/ipinip.go
+++ ipinip/ipinip.go
@@ -2,6 +2,7 @@ package ipinip
 
 import (
 	"encoding/binary"
+	"errors"
 	"net"
 )
 
@@ -48,6 +49,30 @@ func (h IPHeader) Serialize() []byte {
 	copy(b[16:], h.DstAddress.To4())
 
 	return b
+}
+
+// Deserialize parses given slice of byte and returns the IPHeader struct.
+// If some error occured while parsing, empty IPHeader struct and error will be returned.
+func Deserialize(data []byte) (IPHeader, error) {
+	if len(data) < 20 {
+		return IPHeader{}, errors.New("minimum length of IPv4 header is 20 bytes")
+	}
+
+	hdr := IPHeader{}
+	hdr.Version = data[0] >> 4
+	hdr.IHL = data[0] & 0x0f
+	hdr.TypeOfService = data[1]
+	hdr.TotalLength = binary.BigEndian.Uint16(data[2:])
+	hdr.Identification = binary.BigEndian.Uint16(data[4:])
+	hdr.Flags = data[6] >> 5
+	hdr.FlagmentOffset = uint16(data[6]&0x1f)<<8 | uint16(data[7])
+	hdr.TimeToLive = data[8]
+	hdr.Protocol = data[9]
+	hdr.HeaderChecksum = binary.BigEndian.Uint16(data[10:])
+	hdr.SrcAddress = net.IPv4(data[12], data[13], data[14], data[15])
+	hdr.DstAddress = net.IPv4(data[16], data[17], data[18], data[19])
+
+	return hdr, nil
 }
 
 func Encapsulate(data []byte, srcIP net.IP, dstIP net.IP) []byte {
